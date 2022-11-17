54390e5e041265ec2de6a1e57c8ba980336e4c4c
Ángel <angelpola3@hotmail.com>
2022-11-17 20:14:26 +0100

    17/11/2022

--- database/seeders/CasasSeeder.php
+++ database/seeders/CasasSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use \Illuminate\Support\Facades\DB;
 class CasasSeeder extends Seeder
 {
     /**
@@ -14,21 +14,13 @@ class CasasSeeder extends Seeder
     public function run()
     {
         DB::table('casas')->insert([
-            'user_id'=>\App\Models\User::all()->where('name','<>','admin')->random()->id,
+            'user_id'=>'\App\Models\User::all()->where('name','<>','admin')->random()->id',
             'nombre'=> 'Urbanizacion de Lujo',
-            'tipo'=> 'Piso',
+            'dueño'=> 'Piso',
+            'descripcion'=>'Descripcion',
             'direccion'=>'La manjoya 22,3A',
-            'precio'=>'150000',
-            'imagen' => "images/imagen1.jpg",
-        ]);
-
-        DB::table('casas')->insert([
-            'user_id'=>\App\Models\User::all()->where('name','<>','admin')->random()->id,
-            'nombre'=> 'Chalet',
-            'tipo'=> 'Casa',
-            'direccion'=>'Avenida Luis 201,Oviedo',
-            'precio'=>'300000',
-            'imagen'=> 'images/imagen2.jpg',
+            'precio' => "20",
+            'imagen' => "prueba"
         ]);
     }
 }
